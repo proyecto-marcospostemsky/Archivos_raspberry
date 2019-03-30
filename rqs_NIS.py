80f7eefac98bd09955007e36814c5af00a2bae9c
marcospostemsky <marcos.postemsky@gmail.com>
2019-03-30 15:18:59 -0300

    Request nis pero para que el usuario lo configure manualmente.

--- rqs_NIS.py
+++ rqs_NIS.py
@@ -6,86 +6,114 @@ import paho.mqtt.client as mqtt
 #Variables locales al archivo
 ref="San Luis Norte"
 broker="0"*3
+nis_rqs=FFFFFFFFFE
 PyLora.init()
 PyLora.set_frequency(434000000)
 client=mqtt.Client(client_id='raspberry',clean_session=False)
 client.connect("localhost",1883,60)
 
+
+#Comienza por pedir el ingreso del NIS al usuario, verificando la disponibilidad del mismo.
+
 print("Para grabar el identificador NIS en el dispositivo siga los siguientes pasos: \n")
-print(" 1. Ingrese el NIS que se desee grabar. \n")
-print(" 2. Energice el medidor y espere el mensaje de confirmación. \n")
+print("   1. Ingrese el NIS que se desee grabar. \n")
+print("   2. Energice el medidor y espere el mensaje de confirmación. \n")
 
-    PyLora.receive()   # put into receive mode
-    while not PyLora.packet_available():
-        # wait for a package
-        time.sleep(0)
-    rec = PyLora.receive_packet()
-    print(rec)
-    #Verifica que rec no esté vacio, para evitar errores de Types
-    if rec is not None:
-        rec_rec=rec[4:len(rec)]
-        print(rec) #Muestro el dato recibido
-        print(len(rec))
-    if(rec=="RQS NIS"):
-        #Abre archivo, y lee cada una de las líneas
-        
-        file_direcciones=open("mod_address.txt","r+")
-        lista_NIS=file_direcciones.readlines()
-        #El archivo se encuentra vacio?
-        if len(lista_NIS)<1:
-            grabar_archivo=broker+"0"*7+"-San Luis Norte"
-            file_direcciones.write(grabar_archivo)
-            NIS=grabar_archivo[0:10]
-        else:
-    ##        Toma ultimo NIS del archivo de texto, y se le
-    ##        suma uno (al num de modulo) el num de broker se
-            ult_linea=str(lista_NIS[len(lista_NIS)-1])
-            div_ult_linea=ult_linea.split("-")
-            num=int(div_ult_linea[0][3:10])+1
-            NIS=broker+"0"*(7-len(str(num)))+str(num)
-            file_direcciones.writelines("\n"+NIS+"-"+ref)
-            
-        #Guardo archivo con NIS  
-        file_direcciones.close()
+#Variables de control
+NIS="F"
+nis_correcto=0
+reservados={"FFFFFFFFFF","FFFFFFFFFE"}
+disp="0123456789ABCDEF"
+control_caract=1
 
-        time.sleep(0.05)
-        envio_OK=0
-##        print(NIS)
-        #PyLora.send_packet(NIS)
-    
+#Loop para corroborar que el NIS que ingresa el usuario es válido.
+while(len(NIS)!=10 or nis_correcto==0):
+    nis_correcto=1
+    NIS= input("Ingrese el nuevo valor de NIS: ")
+    if(len(NIS)!=10):
+        print("\nEl identificador NIS no tiene el largo correcto\n")
+        nis_correcto=0
+    else:
 
         
-        while(envio_OK==0):
-            #Se debe definir un tiempo de espera por si falla el envio
-            #para volver a reenviar el NIS
-            
-            #Envío del NIS al medidor
-            PyLora.send_packet("FFFFFFFFFF"+NIS)
-                      
-            
-            #Espero respuesta
-            PyLora.receive()   # put into receive mode
-            while not PyLora.packet_available():
-                # wait for a package
-                time.sleep(0)
-            rec = PyLora.receive_packet()
+        for carac in NIS:           #Verifica que los caracteres sean correctos
+            if disp.find(carac)==-1:
+                print("\nSolo se permiten los caracter (0 al 9) y (A a F).\n")
+                nis_correcto=0
+                break
 
-            if rec is not None:
-                rec_rec=rec[4:len(rec)]
-                print(rec)
-                print(len(rec))
-            
-            if "OK-"+NIS==rec:
-##                print("Evento terminado correctamente")
-                envio_OK=1
-            
+        #Se abre el archivo donde se encuentran los NIS, para verificar que no esté en uso.
+        nis_file=open("mod_address.txt","r") #abro archivo con los nis
+        lines=nis_file.read() #Lee todas las lineas del archivo
+        nis_file.close()
+        if nis_correcto==1 and lines.find(NIS)!= -1:
+            print("\nEl NIS ingresado ya se encuentra en uso, por favor de verificarlo.\n")
+            nis_correcto=0
 
+        #Verificación de que el NIS no sea uno de los reservados
+        elif NIS in reservados and nis_correcto==1:
+            print("\nEl NIS ingresado se encuentra reservado.\n")
+            nis_correcto=0
+
+        #Si hasta este punto de control el NIS ha sido correcto, se pide una refencia 
+        #con la cual guardar la información, luego de grabar en el medidor.
+        elif nis_correcto==1:
+            referencia=input("\nNIS disponible. Escriba una referencia: ")
 
 
-    #separo la información que envía el dispositivo
-    #topic_m=str(msg_div[0])
-    #dato=str(msg_div[1])
+print("\n Energice el medidor y espere el mensaje de confirmación.")
 
-    #utilizando mqtt lo publico con mosquitto
-    #client.publish(topic=topic_m,payload=dato)
+
+
+#Se comienza por grabar el NIS del nuevo dispositivo.
+PyLora.receive()   # put into receive mode
+while not PyLora.packet_available():
+    # wait for a package
+    time.sleep(0)
+rec = PyLora.receive_packet()
+print(rec)
+#Verifica que rec no esté vacio, para evitar errores de Types
+if rec is not None:
+    rec_rec=rec[4:len(rec)]
+    print(rec) #Muestro el dato recibido
+    print(len(rec))
+
+
+if(rec=="RQS NIS"):
+    time.sleep(0.05)
+    envio_OK=0
+
+    while(envio_OK==0):
+        #Se debe definir un tiempo de espera por si falla el envio
+        #para volver a reenviar el NIS
+        
+        #Envío del NIS al medidor
+        PyLora.send_packet("FFFFFFFFFF"+NIS)
+                  
+        
+        #Espero respuesta
+        PyLora.receive()   # put into receive mode
+        while not PyLora.packet_available():
+            # wait for a package
+            time.sleep(0)
+        rec = PyLora.receive_packet()
+
+        if rec is not None:
+            rec_rec=rec[4:len(rec)]
+            print(rec)
+            print(len(rec))
+        
+        if "OK-"+NIS==rec:
+            print("Evento terminado correctamente")
+            envio_OK=1
+
+
+#Si se confirma que el medidor recibio el valor configurado, se guarda el NIS en el archivo "mod_address.txt"           
+if(envio_OK==1):
+    nis_file=open("mod_address.txt","r+")
+    lines=nis_file.read() #Lee todas las lineas del archivo
+    nis_file.writelines("\n"+NIS+"-"+referencia) #Guardo nuevo NIS en archivo
+    nis_file.close()
+
+
 
